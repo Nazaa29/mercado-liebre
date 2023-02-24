d43414f3826e3a569c9ae05097b78479395c6f05
Nazaa29 <lopeznazarenoo@gmail.com>
2023-02-24 01:25:46 -0500

    correccion app.js

--- src/app.js
+++ src/app.js
@@ -4,20 +4,20 @@ const app=express();
 
 const port = process.env.PORT || 3002;
 
-app.use(express.static(path.join(__dirname,"../../Mercado Libre")));
+app.use(express.static(path.join(__dirname,"../public")));
 
 app.listen(port, ()=>{
-    console.log("Corriendo");
+    console.log("Corriendo en el puerto " + port);
 });
 
 app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname,"./views/home.html"));
+    res.sendFile(path.join(__dirname,"/views/home.html"));
 });
 
-app.get("/register.html", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/register.html"))
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
 });
 
-app.get("/login.html", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/login.html"))
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
 });
